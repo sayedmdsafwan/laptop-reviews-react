78812c9bfd2b823ad8309c540f0ecb22a1190555
Md. Safwan <sayedmdsafwan@gmail.com>
2022-04-04 02:24:05 +0600

    fix responsiveness

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import image from "../../assets/lapt-removebg.png";
 import useReviews from "../../hooks/useReviews";
 import Review from "../Review/Review";
@@ -8,7 +9,7 @@ const Home = () => {
     return (
         <div className="w-4/5 mx-auto">
             {/* header part complete */}
-            <div className="grid grid-cols-2 py-24 items-center  ">
+            <div className="grid lg:grid-cols-2 grid-cols-1 py-24 items-center  ">
                 <div>
                     <h1 className="text-4xl font-bold uppercase tracking-wide my-4 ">
                         See all reviews about our dell laptop
@@ -28,16 +29,24 @@ const Home = () => {
             </div>
 
             {/* reviews part  */}
-            <div className="cards">
+            <div className="mb-8">
                 <h1 className="text-4xl font-bold uppercase tracking-wider text-center">
                     Reviews Section
                 </h1>
-                <div className="grid grid-cols-3">
+                <div className="grid lg:grid-cols-3 grid-cols-1">
                     {reviews
                         .map((review) => (
                             <Review review={review} key={review.id}></Review>
                         ))
                         .slice(0, 3)}
+                </div>
+                <div className="flex justify-center">
+                    <Link
+                        to="/reviews"
+                        className="px-6 py-2 uppercase my-4 tracking-wider rounded bg-slate-200 duration-200 hover:bg-black hover:text-white"
+                    >
+                        See More Reviews
+                    </Link>
                 </div>
             </div>
         </div>
--- src/components/Reviews/Reviews.js
+++ src/components/Reviews/Reviews.js
@@ -6,7 +6,7 @@ const Reviews = () => {
     const [reviews, setReviews] = useReviews();
 
     return (
-        <div className="w-4/5 mx-auto my-8 grid grid-cols-3">
+        <div className="w-4/5 mx-auto my-8 grid lg:grid-cols-3 grid-cols-1">
             {reviews.map((review) => (
                 <Review review={review} key={review.id}></Review>
             ))}
